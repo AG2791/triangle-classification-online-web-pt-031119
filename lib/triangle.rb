9ca6bcb9f583e60ebf8de00cee417e4dfc6897f6
AJ Guiles <ashiguiles@gmail.com>
2019-04-24 04:24:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  
+  
+  attr_accessor :equilateral,:isosceles, :scalene 
+  
+  def kind
+    @kind = kind
+  end
+  
+  def side_one
+    
+  end
+
+  def side_two
+   
+  end
+
+  def side_three
+  
+   end
+
 end
